047f31de31dcda1207d4f560a3e481f9e9c39ed0
bettencourt-c <catarina.bettencourt.a@gmail.com>
2024-11-09 16:39:22 -0500

    Update fig5.js

--- fig5.js
+++ fig5.js
@@ -1,7 +1,7 @@
 // using the following following template: https://observablehq.com/@d3/zoomable-sunburst
 
 d3.json('drug.json').then(data => {
-    const width = 1000;
+    const width = 1200;
     const height = width;
     const radius = width / 5;
 
