0b38ae599f50089c956818d1265765f6db4aa71f
bettencourt-c <catarina.bettencourt.a@gmail.com>
2024-11-13 09:22:00 -0500

    Update fig5.js

--- fig5.js
+++ fig5.js
@@ -138,7 +138,7 @@ d3.json('drug.json').then(data => {
     }
 
     function labelVisible(d) {
-        return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+        return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.1;
     }
 
     function labelTransform(d) {
